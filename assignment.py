0f718e376ca92e4d23d372cbbf4b35a385154454
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-11-04 09:54:15 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,12 +1,16 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def print_pattern_1(n):
+def find_average(numbers):
     # Write your code here
     pass
 
 # Exercise 2
-def print_pattern_2(n):
+def multiply_elements(numbers):
     # Write your code here
     pass
 
+# Exercise 3
+def find_smallest(numbers):
+    # Write your code here
+    pass
